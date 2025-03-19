9bcfd34da505092100f1e8ca6084e634b828776d
Sajjad Asadi <Asdy47496@gmail.com>
2025-03-19 10:06:11 +0330

    Update MusicModel.kt

--- app/src/main/java/ir/sajjadasadi/RitmoPlayer/MusicModel.kt
+++ app/src/main/java/ir/sajjadasadi/RitmoPlayer/MusicModel.kt
@@ -1,5 +1,13 @@
 package ir.sajjadasadi.RitmoPlayer
 
 import android.net.Uri
+import java.util.Date
 
-data class MusicItem(val title: String, val artist: String, val uri: Uri, val albumArtUri: Uri?)
+data class MusicItem(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val uri: Uri,
+    val albumArtUri: Uri?,
+    val date: Date
+)
